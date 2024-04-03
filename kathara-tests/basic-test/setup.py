7d727b6c48c399a71a90f3a4bd6dcf73d96a2d7e
rgyhuang <roygyhuang@gmail.com>
2024-04-03 10:58:42 -0400

    Implemented topology setup

--- kathara-tests/basic-test/setup.py
+++ kathara-tests/basic-test/setup.py
@@ -7,7 +7,7 @@ import argparse
 from Kathara.model.Lab import Lab
 from Kathara.manager.Kathara import Kathara
 from Kathara.model.Machine import Machine
-
+from collections import defaultdict
 
 def dijkstra(graph, start):
     """
@@ -42,7 +42,6 @@ def dijkstra(graph, start):
     # Return the distance dictionary
     return dist
 
-
 def build_image(img_name, img_path):
     """
     Build docker image
@@ -72,7 +71,7 @@ def setup_device(lab, device_name, image, links, config):
     lab.create_file_from_list(config, f"{device.name}.startup")
     return device
 
-def create_device(lab, device_name, image, link, ip):
+def create_device(lab, device_name, image, links, cmds):
     """
     Create and start device
     :param lab (Kathara.model.Lab): Kathara lab scenario
@@ -83,35 +82,10 @@ def create_device(lab, device_name, image, link, ip):
     :param ip: ip address of node on link
     :return: None
     """
-    print(f'Setting up device {device_name} with arguments ({image}, {link}, {ip})')
-    cmd = [f'ip address add {ip}/24 dev eth0', f'ip route add default via {ip} dev eth0',]
-    links = [link]
-    device = setup_device(lab, device_name, image, links, cmd)
+    print(f'Setting up device {device_name} with arguments ({image}, {links}, {cmds})')
+    
+    device = setup_device(lab, device_name, image, links, cmds)
     print(f'Succesfully added device {device_name}')
-
-
-def create_subnet(ip_range, subnet_name):
-    """
-    Create subnet
-    :param ip_range: range of ips on subnet
-    :param subnet_name: name of subnet
-    :return: None
-    """
-    cmd = f"docker network create --subnet={ip_range} {subnet_name}"
-    print(cmd)
-    os.system(cmd)
-
-
-def remove_subnet(subnet_name):
-    """
-    Remove subnet
-    :param subnet_name: name of subnet
-    :return: None
-    """
-    cmd = f"docker network rm {subnet_name}"
-    print(cmd)
-    os.system(cmd)
-
 
 def attach(lab, ip, link_name, device_name, interface, tc_params):
     """
@@ -123,18 +97,7 @@ def attach(lab, ip, link_name, device_name, interface, tc_params):
     :param tc_params: tuple (bandwidth, burst, latency)
     :return: None
     """
-    if interface == "eth0":
-        configure_link(lab, device_name, interface, tc_params)
-    else:
-        print(interface)
-        cmd = f"/sbin/ifconfig {interface} {ip} up"
-        (stdout, stderr, cmdValue) = Kathara.get_instance().exec(lab_hash=lab.hash, machine_name=device_name, command=cmd, stream=False, wait=True)
-
-        cmd = f"ip address add {ip}/24 dev {interface}"
-        print(cmd)
-        (stdout, stderr, cmdValue) = Kathara.get_instance().exec(lab_hash=lab.hash, machine_name=device_name, command=cmd, stream=False, wait=True)
-        print(stdout, stderr, cmdValue)
-        configure_link(lab, device_name, interface, tc_params)
+    configure_link(lab, device_name, interface, tc_params)
 
 
 def remove_link(lab, link):
@@ -173,17 +136,12 @@ def add_route(lab, device_name, ip_range, gateway_ip, interface):
     cmd = f"ip route add {ip_range}" \
           f" via {gateway_ip} dev {interface}"
     (stout, stderr, cmdValue) = Kathara.get_instance().exec(lab_hash=lab.hash, machine_name=device_name, command=cmd, stream=False, wait=True)
-    print(stout)
-    print(stderr)
-    print(cmdValue)
+
     if cmdValue != 0:
         cmd = f"ip route change {ip_range}" \
               f" via {gateway_ip} dev {interface}"
 
         (stout, stderr, cmdValue) = Kathara.get_instance().exec(lab_hash=lab.hash, machine_name=device_name, command=cmd, stream=False, wait=True)
-        print(stout)
-        print(stderr)
-        print(cmdValue)
 
 
 
@@ -210,7 +168,7 @@ def write_state_json(nodes, links, node_vs_ip, node_vs_eth):
     :param: node_vs_eth: the highest ethernet interface number used.
     :return: the current state of the graph in dictionary format
     """
-    with open("../tmp/state.json", "w") as f:
+    with open("./tmp/state.json", "w") as f:
         json.dump({"nodes": nodes, "links": links, "node_vs_ip": node_vs_ip, "node_vs_eth": node_vs_eth}, f, indent=4)
 
 
@@ -220,7 +178,7 @@ def read_state_json():
     :param: None
     :return: the current state of the graph in dictionary format
     """
-    with open("../tmp/state.json", "r") as f:
+    with open("./tmp/state.json", "r") as f:
         current_state = json.load(f)
     return current_state
 
@@ -271,8 +229,6 @@ def configure_link(lab, node, interface, tc_params):
     (stdout1, stderr1, cmd_value_bandwidth) = Kathara.get_instance().exec(lab_hash=lab.hash, machine_name=node, command=cmd_bandwidth, stream=False, wait=True)
     (stdout2, stderr2, cmd_value_latency) = Kathara.get_instance().exec(lab_hash=lab.hash, machine_name=node, command=cmd_latency, stream=False, wait=True)
 
-    print(stdout1, stderr1, cmd_value_bandwidth)
-    print(stdout2, stderr2, cmd_value_latency) 
     # Error handling for cmd_bandwidth and cmd_latency
     if cmd_value_bandwidth != 0 or cmd_value_latency != 0:
         clear_child = f"tc qdisc del dev {interface} parent 1:1 handle 10"
@@ -338,6 +294,8 @@ def main(args):
         # Reading and storing information from the config.py file
         if args.config is not None:
             config = importlib.import_module(args.config)
+            start_up_cmds = defaultdict(list)
+            start_up_links = defaultdict(list)
             node_vs_ip = {}
             node_vs_eth = {}
             for node_name, node_param in config.nodes.items():
@@ -348,22 +306,32 @@ def main(args):
             links = {}
             for link_info in config.links:
                 link_name, node_vs_eth, link_param = generate_link_param(node_vs_eth, link_info)
+    
+                source, dest = link_param[1][0][0], link_param[1][1][0]
+
+                # set ip addresses for interfaces
+                cmd_source = f'ip address add {link_param[1][0][1]}/24 dev {link_param[1][0][2]}'
+                cmd_dest = f'ip address add {link_param[1][1][1]}/24 dev {link_param[1][1][2]}'
+                start_up_cmds[source].append(cmd_source)
+                start_up_cmds[dest].append(cmd_dest)
+
+                # add collision domains
+                start_up_links[source].append(link_name)
+                start_up_links[dest].append(link_name)
                 links[link_name] = link_param
+
             nodes = config.nodes
-            print(nodes)
-            # print(links)
-            # build image for node
+
+            # uncomment to rebuild
             # build_image("katharatestimage", ".")
 
             # create devices
             for node_name, node_param in nodes.items():
-                create_device(lab, node_name, "katharatestimage", node_param[1], node_param[0])
+                create_device(lab, node_name, "katharatestimage", start_up_links[node_name], start_up_cmds[node_name])
+ 
             Kathara.get_instance().deploy_lab(lab)
-            # # create subnets
-            # for link_name, link_param in links.items():
-            # 	create_subnet(link_param[0], link_name)
 
-            # attach containers to networks
+            # attach devices to collision domains
             for link_name, link_param in links.items():
                 endpoints = link_param[1]
                 tc_params = link_param[2]
@@ -377,57 +345,59 @@ def main(args):
 
 
 
-        # # Using Dijkstra to configure routing tables with add route function above
-        # graph = {}
-        # connections = {}
-        # # Create Graph
-        # for link_name, link_param in links.items():
-        # 	endpoints = link_param[1]
-        # 	tc_params = link_param[2]
+        # Using Dijkstra to configure routing tables with add route function above
+        graph = {}
+        connections = {}
+        # Create Graph
+        for link_name, link_param in links.items():
+            endpoints = link_param[1]
+            tc_params = link_param[2]
 
-        # 	if endpoints[0][0] not in node_vs_ip:
-        # 		node_vs_ip[endpoints[0][0]] = []
-        # 	node_vs_ip[endpoints[0][0]].append(endpoints[0][1])
-        # 	if endpoints[1][0] not in node_vs_ip:
-        # 		node_vs_ip[endpoints[1][0]] = []
-        # 	node_vs_ip[endpoints[1][0]].append(endpoints[1][1])
+            if endpoints[0][0] not in node_vs_ip:
+                node_vs_ip[endpoints[0][0]] = []
+            node_vs_ip[endpoints[0][0]].append(endpoints[0][1])
+            if endpoints[1][0] not in node_vs_ip:
+                node_vs_ip[endpoints[1][0]] = []
+            node_vs_ip[endpoints[1][0]].append(endpoints[1][1])
 
-        # 	if endpoints[0][0] not in graph:
-        # 		graph[endpoints[0][0]] = []
-        # 		connections[endpoints[0][0]] = {}
-        # 	graph[endpoints[0][0]].append((endpoints[1][0], tc_params))
-        # 	connections[endpoints[0][0]][endpoints[1][0]] = (
-        # 		endpoints[1][1], endpoints[0][2])  # ip of other node,eth of itself
-        # 	if endpoints[1][0] not in graph:
-        # 		graph[endpoints[1][0]] = []
-        # 		connections[endpoints[1][0]] = {}
-        # 	graph[endpoints[1][0]].append((endpoints[0][0], tc_params))
-        # 	connections[endpoints[1][0]][endpoints[0][0]] = (endpoints[0][1], endpoints[1][2])
+            if endpoints[0][0] not in graph:
+                graph[endpoints[0][0]] = []
+                connections[endpoints[0][0]] = {}
+            graph[endpoints[0][0]].append((endpoints[1][0], tc_params))
+            connections[endpoints[0][0]][endpoints[1][0]] = (
+                endpoints[1][1], endpoints[0][2])  # ip of other node,eth of itself
+            if endpoints[1][0] not in graph:
+                graph[endpoints[1][0]] = []
+                connections[endpoints[1][0]] = {}
+            graph[endpoints[1][0]].append((endpoints[0][0], tc_params))
+            connections[endpoints[1][0]][endpoints[0][0]] = (endpoints[0][1], endpoints[1][2])
 
-        # for start_node in graph:
-        # 	dist = dijkstra(graph, start_node)
-        # 	hops = []
-        # 	for node in graph:
-        # 		if node == start_node:
-        # 			continue
-        # 		prev_node = dist[node][1]
-        # 		next_hop = node
-        # 		while prev_node != start_node and dist[prev_node][0] != float('inf'):
-        # 			next_hop = prev_node
-        # 			prev_node = dist[prev_node][1]
-        # 		if dist[prev_node][0] == float('inf'):
-        # 			continue
-        # 		hops.append((node, next_hop))
-        # 	print(f"\nStart Node = {start_node}")
-        # 	for dest_node, next_hop_node in hops:
-        # 		next_hop_node_ip = connections[start_node][next_hop_node][0]
-        # 		interface = connections[start_node][next_hop_node][1]
-        # 		for dest_node_ip in node_vs_ip[dest_node]:
-        # 			add_route(lab, start_node, dest_node_ip, next_hop_node_ip, interface)
-        # 		print(f"Destination Node = {dest_node}, Next hop = {next_hop_node}")
+        for start_node in graph:
+            dist = dijkstra(graph, start_node)
+            hops = []
+            for node in graph:
+                if node == start_node:
+                    continue
+                prev_node = dist[node][1]
+                next_hop = node
+                while prev_node != start_node and dist[prev_node][0] != float('inf'):
+                    next_hop = prev_node
+                    prev_node = dist[prev_node][1]
+                if dist[prev_node][0] == float('inf'):
+                    continue
+                hops.append((node, next_hop))
+            print(f"\nStart Node = {start_node}")
+            for dest_node, next_hop_node in hops:
+                next_hop_node_ip = connections[start_node][next_hop_node][0]
+                interface = connections[start_node][next_hop_node][1]
+                for dest_node_ip in node_vs_ip[dest_node]:
+                    print(dest_node_ip)
+                    print(next_hop_node_ip)
+                    add_route(lab, start_node, dest_node_ip, next_hop_node_ip, interface)
+                print(f"Destination Node = {dest_node}, Next hop = {next_hop_node}")
 
-        # # # Store the current state to state.json file
-        # write_state_json(nodes, links, node_vs_ip, node_vs_eth)
+        # Store the current state to state.json file
+        write_state_json(nodes, links, node_vs_ip, node_vs_eth)
         Kathara.get_instance().undeploy_lab(lab_name=lab.name)
     except Exception as e:
         print(e)
